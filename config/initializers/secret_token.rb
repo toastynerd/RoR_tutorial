8b7c5bd6c09fd3e2186a07542021da3f46b24ce5
toasty <toasty@ubuntu-server.(none)>
2013-04-06 09:12:04 -0700

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RoRTutorialApp::Application.config.secret_token = '872699578cdb06895a0f7b595ee8e8144c85f7813292355881522a081508f2ae01a547cab6082b1bcaf85996b5af2e65996fed56aa35d1ec1d8a21b6b521f193'
